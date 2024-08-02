a9af822ed004c4bc73c6234410f1488cb6cbd227
Viswadeep Manam <manam.viswadeep@gmail.com>
2024-08-02 01:36:37 -0400

    Create sqs queues on aws

--- path-tracer-preprocessor/preprocessor-function/app.py
+++ path-tracer-preprocessor/preprocessor-function/app.py
@@ -6,26 +6,69 @@ import os
 
 from preprocess.preprocessor import Preprocessor
 
-def create_topic(env, config, topic_name: str):
-    session = boto3.session.Session()
-    
-    sns_config = boto3.session.Config(proxies={"http": config['sns']['url']}) if env == 'local' else None
-    sns_client = session.client(
-        service_name='sns',
-        region_name=config['sns']['region'],
-        config=sns_config
-    )
-    
+def create_topic(sns_client, topic_name: str):
     try:
         response = sns_client.create_topic(Name=topic_name)
+        return response
     except Exception as e:
         print("Error creating topic: {}".format(e))
         
-    return response
-
-def create_queues(worker_ids: List[int]):
-
-    pass
+def create_queues(sns_client, sqs_client, topic_arn, scene_name, worker_ids: List[int]):
+    def create_and_subscribe_worker_queue(worker_id: str) -> str:
+        try:
+            response = sqs_client.create_queue(QueueName='{}-worker-{}'.format(scene_name, worker_id))
+            queue_url = response['QueueUrl']
+            queue_attributes = sqs_client.get_queue_attributes(QueueUrl=queue_url, AttributeNames=['QueueArn'])
+            queue_arn = queue_attributes['Attributes']['QueueArn']
+            
+            filter_policy = {'worker_id': [worker_id]} if worker_id == 'master' else {"worker_id" : [worker_id, "ALL"]}
+            sns_client.subscribe(
+                TopicArn=topic_arn,
+                Protocol='sqs',
+                Endpoint=queue_arn,
+                Attributes={
+                    'FilterPolicy': json.dumps(filter_policy)
+                }
+            )
+            
+            queue_policy = {
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": { "Service": "sns.amazonaws.com" },
+                        "Action": "sqs:SendMessage",
+                        "Resource": queue_arn,
+                        "Condition": {
+                            "ArnEquals": {
+                                "aws:SourceArn": topic_arn
+                            }
+                        }
+                    }
+                ]
+            }
+            
+            sqs_client.set_queue_attributes(
+                QueueUrl=queue_url,
+                Attributes={'Policy': json.dumps(queue_policy)}
+            )
+            
+            return queue_url
+        except Exception as e:
+            print("Error creating queue: {}".format(e))
+        
+    worker_queues = {}
+    
+    master_queue_arn = create_and_subscribe_worker_queue('master')
+    worker_queues['master'] = master_queue_arn
+            
+    for worker_id in worker_ids:
+        try:
+            queue_arn = create_and_subscribe_worker_queue(str(worker_id))
+            worker_queues[worker_id] = queue_arn
+            return worker_queues
+            
+        except Exception as e:
+            print("Error creating queue: {}".format(e))
 
 def lambda_handler(event, context):
     function_input = event['body']
@@ -34,8 +77,7 @@ def lambda_handler(event, context):
     scene_key = function_input['scene_key']
     scene_name = function_input['scene_name']
     
-    config_file = "prod.yml" if os.environ.get('prod') else "local.yml"
-    env = 'prod' if os.environ.get('prod') else 'local'
+    config_file = "prod.yml" 
     
     with open(config_file, 'r') as stream:
         config = yaml.safe_load(stream)
@@ -43,10 +85,68 @@ def lambda_handler(event, context):
     preprocessor = Preprocessor(scene_bucket=scene_bucket, scene_root=scene_key)    
     split_scene = preprocessor.get_split_scene()
     
-    sns_response = create_topic(env, config, '{}-topic'.format(scene_name))
+    session = boto3.session.Session()
+    
+    sns_client = session.client(
+        service_name='sns',
+        region_name=config['sns']['region'],
+    )
+    
+    
+    sqs_client = boto3.client(
+        service_name='sqs',
+        region_name=config['sqs']['region'],
+    )
+        
+    sns_response = create_topic(sns_client, '{}-topic'.format(scene_name))
     print(sns_response)
+    
+    topic_arn = sns_response['TopicArn']
 
+    try:
+        worker_queues = create_queues(sns_client, sqs_client, topic_arn, scene_name, split_scene['split_work'].keys())
+    
+        output = {
+            "scene": split_scene,
+            "queues": worker_queues
+        }
+    
+        message = {
+            "worker_id": "1",
+            "message": "Test"
+        }
+    
+        response = sns_client.publish(
+            TargetArn=topic_arn,
+            Message=json.dumps({'default': json.dumps(message)}),
+            MessageStructure='json'
+        )
+    
+        message = {
+            "worker_id": "master",
+            "message": "Test for master"
+        }
+    
+        response = sns_client.publish(
+            TargetArn=topic_arn,
+            Message=json.dumps({'default': json.dumps(message)}),
+            MessageStructure='json'
+        )
+    
+        message = {
+            "worker_id": "ALL",
+            "message": "Test for ALL"
+        }
+    
+        response = sns_client.publish(
+            TargetArn=topic_arn,
+            Message=json.dumps({'default': json.dumps(message)}),
+            MessageStructure='json'
+        )
+    
+    except Exception as e:
+        print(e)
     return {
         "statusCode": 200,
-        "body": json.dumps(split_scene),
+        "body": json.dumps(output),
     }
