c9ebaeeef7eac37e24a90ef47f42858789723079
Viswadeep Manam <manam.viswadeep@gmail.com>
2025-12-05 22:16:35 +0000

    Remove lambda handler for worker

--- path-tracer-core/src/main.cpp
+++ path-tracer-core/src/main.cpp
@@ -4,17 +4,24 @@
 #include "processors/application.hpp"
 #include "processors/master/master.hpp"
 #include "processors/worker/worker.hpp"
+#include <stdexcept>
 
 using json = nlohmann::json;
 
-aws::lambda_runtime::invocation_response my_handler(aws::lambda_runtime::invocation_request const &request)
+void run()
 {
     Aws::SDKOptions options;
     try
     {
         Aws::InitAPI(options);
 
-        const std::string &payload = request.payload;
+        const char *env_p = std::getenv("WORKER_INFO");
+        if (env_p == nullptr)
+        {
+            throw std::runtime_error("WORKER_INFO environment variable is not set");
+        }
+
+        const std::string &payload = env_p;
         json worker_info_json = json::parse(payload);
         models::worker_info info = worker_info_json.get<models::worker_info>();
 
@@ -28,33 +35,24 @@ aws::lambda_runtime::invocation_response my_handler(aws::lambda_runtime::invocat
 
         app->run();
         Aws::ShutdownAPI(options);
-        return aws::lambda_runtime::invocation_response::success("Render Complete!", "application/json");
-    }
-    catch (const std::system_error &e)
-    {
-        spdlog::error("System error: {}, error code: {}, category: {}", e.what(), e.code().value(),
-                      e.code().category().name());
-        Aws::ShutdownAPI(options);
-        return aws::lambda_runtime::invocation_response::failure(fmt::format("System error: {}", e.what()),
-                                                                 "SystemError");
     }
     catch (const std::exception &e)
     {
         spdlog::error("Exception caught: {}", e.what());
         Aws::ShutdownAPI(options);
-        return aws::lambda_runtime::invocation_response::failure(fmt::format("Exception: {}", e.what()),
-                                                                 "RuntimeError");
+        std::runtime_error ex(fmt::format("Exception caught: {}", e.what()));
+        throw ex;
     }
     catch (...)
     {
         spdlog::error("Unknown exception caught");
         Aws::ShutdownAPI(options);
-        return aws::lambda_runtime::invocation_response::failure("Unknown exception occurred", "UnknownError");
+        throw std::runtime_error("Unknown exception caught");
     }
 }
 
 int main()
 {
-    aws::lambda_runtime::run_handler(my_handler);
+    run();
     return 0;
 }
